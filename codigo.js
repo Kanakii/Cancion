94cbb6430e1314251cb108766c7360e7bf1d13e4
Kanakii <adrianvelezcomi@gmail.com>
2023-03-03 15:46:35 +0100

    first commit

---
+++ codigo.js
@@ -0,0 +1,37 @@
+// function mostrar (){
+//     let nombre = prompt("Dime tu nombre");
+
+//     swal('hola'  +  nombre);
+    
+//     swal('Esta canción esta dedicada para ti  ' + nombre + '  ,espero que te guste');
+
+// }
+function mostrar(){
+    let nombre = prompt("Dime tu nombre");
+
+    alert('Esta canción esta dedicada para ti  ' + nombre + '  ,espero que te guste');
+
+    alert('Recuerdo tu cuerpo y me entran los calores');
+    alert('Si tú ya no me quieres yo no quiero amores');
+    alert('Y yo sigo loco por tus huesos');
+    alert('Y tú loca porque me olvide de eso');
+    alert('Te imagino a ti regalando tus besos');
+    alert('Y es como si algo dentro de mí se hubiera muerto');
+    alert('Quiero escaparme del ayer');
+    alert('Pero no puedo desaparecer');
+    alert('Ya no me siento con las ganas');
+    alert('De luchar por ti otra vez');
+    alert('Estoy contigo, me creo inmortal');
+    alert('Mira ese culo debería ser ilegal, prima');
+    alert('Tengo planes de futuro si te animas');
+    alert('Y es que primo hasta sueño con ella');
+    alert('Espero te haya gustado, es un poco cutre pero mañana lo mejoraré si puedo');
+}
+
+
+
+
+
+
+
+
